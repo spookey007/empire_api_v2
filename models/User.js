d138c6ad370a3e9ae2ea00172e2698176001bc60
Fahad Bashir <fahadbashir757@gmail.com>
2024-06-15 22:55:41 +0500

    fixes related to auth

--- models/User.js
+++ models/User.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
   status: { type: Number, default: 1 },
   user_type: { type: Number, required: true },
   date_of_birth: { type: Date, required: true },
-  address: { type: String, required: true },
+  address: { type: String },
   creation_date: { type: Date, default: Date.now }
 });
 
